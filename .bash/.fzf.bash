f4faf5626d07d69aeb736198be25d83970e6f7e1
David Schlosnagle <schlosna@gmail.com>
2023-05-22 18:59:28 -0400

    Update .fzf.bash install path

--- .bash/.fzf.bash
+++ .bash/.fzf.bash
@@ -1,13 +1,16 @@
 # Setup fzf
 # ---------
+
+BREW_HOME=${BREW_HOME:-/opt/homebrew}
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && [ -s "${BREW_HOME}/opt/fzf/shell/completion.bash" ] && . "${BREW_HOME}/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+key_bindings="${BREW_HOME}/opt/fzf/shell/key-bindings.bash"
+[ -s "${key_bindings}" ] && . "${key_bindings}";
